0b039a3d3956e208cc1ce3619feef7b036fe5dac
Brian Hotopp <brihoto@gmail.com>
2022-04-04 13:39:16 -0400

    refactored alignment object to contain functions for getting top shifted words and context for a word, added classmethod function to get example sentences for an alignment

--- demo-b/app/preprocessing/generate_examples/alignment/align.py
+++ demo-b/app/preprocessing/generate_examples/alignment/align.py
@@ -1,9 +1,10 @@
 from time import time
+from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
-import json
 from scipy.spatial.distance import cosine
 from ...mapping import perform_mapping
 from app.preprocessing.WordVectors import WordVectors
+import app.preprocessing.generate_embeddings.occurrences as occ
 from .global_align import GlobalAlignConfig
 from .noise_aware_align import NoiseAwareAlignConfig
 from .s4_align import S4AlignConfig
@@ -12,8 +13,9 @@ from sklearn.metrics.pairwise import paired_cosine_distances
 from sklearn.metrics import pairwise_distances
 
 
+
 class Alignment:
-    def __init__(self, common, v1, v2, shifts, dists):
+    def __init__(self, common, v1, v2, shifts, dists, Q):
         """
         creates an alignment object
         :param common: list of common words
@@ -27,6 +29,37 @@ class Alignment:
         self.v2 = v2
         self.shifts = shifts
         self.dists = dists
+        self.Q = Q
+    def top_shifted_words(self, num_words=10):
+        # get the top shifted words
+        num_words = min(num_words, len(self.shifts)-1)
+        indices = np.argpartition(self.shifts, -1*num_words)[-1*num_words:]
+        top_shifted_words = [(self.common[i], self.shifts[i]) for i in indices]
+        top_shifted_words.sort(key=lambda x: x[1], reverse=True)
+        return top_shifted_words
+    def get_context(self, target, first, num_neighbors = 10):
+        """
+        get the nearest neighbors of the target word in the adjacent context
+        """
+        # this is bad o(n) in the number of words in the intersection
+        # but fixing will require ds refactor
+        # todo
+        wi = self.common.index(target)
+        # get the nearest neighbors
+        if first:
+            # target word is in the first context
+            # find nearest neighbors in the second context
+            indices = np.argpartition(self.dists[wi, :], num_neighbors)[:num_neighbors]
+            r = [(self.common[i], self.dists[wi, i], self.v2[i]) for i in indices]
+        else:
+            # target word is in the second context
+            # find nearest neighbors in the first context
+            indices = np.argpartition(self.dists[:, wi], num_neighbors)[:num_neighbors]
+            r = [(self.common[i], self.dists[i, wi], self.v1[i]) for i in indices]
+        r.sort(key=lambda x: x[1])
+        # return unzipped r
+        words, distances, vectors = zip(*r)
+        return words, distances, vectors
 
     @staticmethod
     def from_wv_and_config(wv1, wv2, config_dict):
@@ -40,7 +73,7 @@ class Alignment:
         cfg_obj = Alignment.config_from_dict(config_dict)
         # intersect wv1, wv2
         wv1, wv2 = WordVectors.intersect(wv1, wv2)
-        wv1_aligned = cfg_obj.align(wv1, wv2)[0]
+        wv1_aligned, _, Q = cfg_obj.align(wv1, wv2)
         # vectors for each word
         v1 = wv1_aligned.vectors
         v2 = wv2.vectors
@@ -50,7 +83,7 @@ class Alignment:
         # semantic shift for each word
         shifts = Alignment.compute_shifts(v1, v2)
         dists = Alignment.compute_dists(v1, v2)
-        return Alignment(common, v1, v2, shifts, dists)
+        return Alignment(common, v1, v2, shifts, dists, Q)
 
     @staticmethod
     def config_from_dict(config_dict):
@@ -88,14 +121,63 @@ class Alignment:
         return r
 
     @staticmethod
-    def compute_dists(v1, v2):
+    def compute_dists(v1, v2, verbose=False):
         """
         computes the distance between each word vector in the two examples
         """
-        # time how long it takes
-        start = time()
+        if verbose:
+            # time how long it takes
+            start = time()
         # compute pairwise euclidean distance
         dists = pairwise_distances(v1, v2, metric="euclidean")
-        end = time()
-        print("Computing pairwise distances took {} seconds".format(end - start))
+        if verbose:
+            end = time()
+            print("Computing pairwise distances took {} seconds".format(end - start))
         return dists
+    @staticmethod
+    def get_example_sentences(target, occ1, occ2, spt_path1, spt_path2, Q, wv1, wv2, max_sent=1000):
+        """
+        gets a pair of example sentences
+        target: word to find example sentences for
+        occ1: dictionary containing the number of times each word occurs in each line of pt1
+        occ2: dictionary containing the number of times each word occurs in each line of pt2
+        spt_path1: path object to the scrubbed plaintext 1
+        spt_path2: path object to the scrubbed plaintext 2
+        Q: rotation matrix of the associated alignment
+        wv1: WordVectors to embed the sentences from pt1 with
+        wv2: WordVectors to embed the sentences from pt2 with
+        max_sent: the maximum number of sentences to return
+        """
+        i1 = occ1[target]
+        i2 = occ2[target]
+        indices1, s1te = occ.embed_lines(spt_path1, i1, wv1)
+        indices2, s2te = occ.embed_lines(spt_path2, i2, wv2)
+        # align embedded sentences using Q
+        s1tea = np.matmul(s1te, Q)
+        max_sent = min(max_sent, len(indices1), len(indices2))
+        indices = np.argpartition(
+            cosine_similarity(s1tea, s2te),
+            max_sent,
+            axis=None,
+            kind="introselect",
+            order=None
+        )
+        indices = indices[:min(len(indices), max_sent)]
+        spt1inds =  []
+        spt2inds = []
+        used_i = set()
+        used_j = set()
+        for ind in indices:
+            i, j = np.unravel_index(ind, (len(s1tea), len(s2te)))
+            if i not in used_i and j not in used_j:
+                spt1inds.append(indices1[i])
+                spt2inds.append(indices2[j])
+                used_i.add(i)
+                used_j.add(j)
+        # zip and return
+        sents = []
+        for i1, i2 in zip(spt1inds, spt2inds):
+            sents.append((occ.line_from_file(spt_path1, i1), occ.line_from_file(spt_path2, i2)))
+        return sents
+
+        
