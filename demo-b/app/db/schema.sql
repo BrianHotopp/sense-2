7d4d330fbceb184be4a6426e5e2179fde80bc058
Brian Hotopp <brihoto@gmail.com>
2022-04-04 13:32:38 -0400

    removed tables for holding embedding vectors, added path pointers to plaintext, embeddings, alignments tables to track generated artifacts

--- demo-b/app/db/schema.sql
+++ demo-b/app/db/schema.sql
@@ -3,28 +3,28 @@ create table plaintexts (
     id integer primary key autoincrement,
     name varchar not null,
     description varchar not null,
+    -- path to the original plaintext
     p_path varchar not null,
-    s_path varchar not null
+    -- path to the scrubbed plaintext
+    s_path varchar not null,
+    -- path to the tokenized plaintext 
+    t_path varchar not null,
+    occ_path varchar not null
+
 );
+
 drop table if exists embeddings;
 create table embeddings (
   id integer primary key autoincrement,
   name varchar not null,
   description varchar not null,
   pt_id integer not null,
+  -- path to the embedding
+  wv_path varchar not null,
   -- foreign key constraint, if we delete plaintext delete associated embeddings
   foreign key (pt_id) references plaintexts(id) on delete cascade
 );
-drop table if exists e_vectors;
-create table e_vectors (
-  e_id integer not null,
-  word varchar not null,
-  vector blob not null,
-  -- primary key on e_id and word
-  primary key (e_id, word)
-  -- foreign key constraints, if we delete the associated embedding delete its vectors
-  foreign key (e_id) references embeddings(id) on delete cascade
-);
+
 drop table if exists alignments;
 create table alignments (
   id integer primary key autoincrement,
@@ -32,20 +32,13 @@ create table alignments (
   description varchar not null,
   e1_id integer not null,
   e2_id integer not null,
+  -- path to the alignment object
+  a_path varchar not null,
   -- foreign key constraints, on delete cascade
   foreign key (e1_id) references embeddings(id) on delete cascade
   foreign key (e2_id) references embeddings(id) on delete cascade
 );
-drop table if exists a_vectors;
-create table a_vectors (
-  a_id integer not null,
-  -- foreign key constraints, on delete cascade
-  first boolean not null,
-  word varchar not null,
-  vector blob not null,
-  primary key (a_id, first, word)
-  foreign key (a_id) references alignments(id) on delete cascade
-);
+
 drop table if exists dists;
 create table dists (
   a_id integer not null,
