e3e42030c52ebf6a54296d7a6b55df46d98ba049
Brian Hotopp <brihoto@gmail.com>
2022-04-19 01:24:20 -0400

    moved mostshifted into its own tab, added number of results input field

---
+++ demo-f/src/components/Tab4/MostShifted.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps(
+    ["alignmentName","words"]
+)
+
+const count = ref(0)
+</script>
+
+<template>
+<div class="card p-2">
+    <h5 style="text-align: left">
+        Alignment type: <b>{{ alignmentName }}</b>
+    </h5>
+    <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Word</th>
+      <th scope="col">Shift (Cosine Distance)</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(word, index) in words" :key="index">
+      <th scope="row">{{index+1}}</th>
+      <td><a href="#!" @click="$emit('selectWord', word[0])">{{word[0]}}</a></td>
+      <td>{{word[1].toFixed(2)}}</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+</template>
+
+<style scoped>
+</style>
