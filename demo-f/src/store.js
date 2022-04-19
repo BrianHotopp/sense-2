9c8b7de515d10ab205eed3aca4c8b0b1cdb5dee0
Brian Hotopp <brihoto@gmail.com>
2022-04-19 01:19:17 -0400

    global store for holding global state (moved out from App.vue)

---
+++ demo-f/src/store.js
@@ -0,0 +1,7 @@
+import { reactive } from 'vue'
+import { Queue } from "./Queue.js";
+export const store = reactive({
+    selectedPlaintexts: new Queue(),
+    selectedEmbeddings: {forPt1: new Queue(), forPt2: new Queue()},
+    selectedAlignments: new Queue(),
+})
