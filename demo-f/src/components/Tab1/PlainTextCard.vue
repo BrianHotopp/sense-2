90f49978f1c9a907b5c76d7a3ce3eefff582e0ff
Brian Hotopp <brihoto@gmail.com>
2022-04-19 01:21:45 -0400

    tab 1 refactored into its own folder

---
+++ demo-f/src/components/Tab1/PlainTextCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
